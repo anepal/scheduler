9904b9c894ff1d50eaa3d1732eadb3bcdf56f296
Brian Kaney <brian@vermonster.com>
2010-09-15 16:53:03 -0400

    Use the more recent syntax for rspec (see: http://pure-rspec-rubynation.heroku.com/).  Try to update doctor_spec.rb to match.

--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -1,18 +1,12 @@
 require 'spec_helper'
 
 describe Department do
-  before(:each) do
-    @valid_attributes = {
-      :name => "OPD",
-      :address => "Kantipath",
-      :phone => "01434343",
-      :optional_info => "additional info"
-    }
-    Department.create(@valid_attributes)
-  end
-
-  it "should enforce the uniqueness of the users' name" do
-    new_user=Department.new(:name=>"OPD")
-    new_user.should_not be_valid
-  end
+  subject { Factory.build(:department) }
+  it { should be_valid }
+  it { should have_many :department_doctors }
+  it { should have_many :doctors }
+  it { 
+    Factory.create(:department)
+    should validate_uniqueness_of :name
+  }
 end
